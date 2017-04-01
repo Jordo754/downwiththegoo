3e21df8311934ce3af55e8c6208aea2c1462dc1a
Tristan meere <tjm9563@rit.edu>
2017-04-01 12:22:08 -0400

    Slime Child Script - Ignores collision between each edge vert and other verts

---
+++ downwiththegoo/Assets/Scripts/SlimeChild.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeChild : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] temp = GameObject.FindGameObjectsWithTag("EdgeVert");
+		for (int i=0; i < temp.Length; i++)
+		{
+			Physics2D.IgnoreCollision(temp[i].GetComponent<Collider2D>(), GetComponent<Collider2D>());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
