62cb6eb876b13180ee197efbc2ef977f7936d49d
Matt Dieselman <mxd2278@rit.edu>
2017-04-02 09:30:39 -0400

    Adjusted mesh, cloth shouldnt fall off now. Added teleporter mechanic.

---
+++ downwiththegoo/Assets/Scripts/InTele.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InTele : MonoBehaviour {
+    public int level = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ downwiththegoo/Assets/Scripts/TeleTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleTrigger : MonoBehaviour {
+    public int level;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().tag == "CenterVert")
+        {
+            Debug.Log("HIT TELE");
+
+            GameObject[] temp = GameObject.FindGameObjectsWithTag("InTele");
+            for(int i=0;i<temp.Length;i++)
+            {
+                Debug.Log("Inside search");
+                Debug.Log(temp[i]);
+                int tempLevel = temp[i].GetComponent<InTele>().level;
+                if (tempLevel == (this.level + 1))
+                {
+                    Debug.Log("Inside if");
+                    GameObject.Find("Player").transform.position = new Vector3(temp[i].transform.position.x,temp[i].transform.position.y,0);
+                }
+            }
+        }
+    }
+}
